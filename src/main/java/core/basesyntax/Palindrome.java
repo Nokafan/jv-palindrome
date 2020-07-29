ea2f5b727366b515766d82909d007e829b8ee354
Nokafan <volodymy.stepura@gmail.com>
2020-07-29 15:03:28 +0300

    Added solution for PalindtomeTest

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -21,6 +21,8 @@ public class Palindrome {
      * <p>Результат: true</p>
      */
     public boolean isPalindrome(String text) {
-        return false;
+        text = text.replaceAll("[\\W]", "");
+        StringBuilder myStringBuilder = new StringBuilder(text);
+        return text.equalsIgnoreCase(myStringBuilder.reverse().toString());
     }
 }
